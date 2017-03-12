9a60e26bc2069145210ac3ce81f0107258fd25f9
kuznetz <s.kuznetzov@mail.ru>
2017-03-12 18:18:29 +0300

    delete method item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,13 +5,9 @@ class ItemsController < ApplicationController
   	render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
   end
   def create
-  	@item = Item.create(item_params)
   	
-  	render text: "#{@item.id}: #{@item.name}(#{!@item.new_record?})"
+  	
+  	render text: "item created"
   end
-  private
-
-  def item_params
-  	params.require(:item).permit(:name,:real,:weight,:description,:price)
-  end
+ 
 end
