d84bb8766f99a0ab70cbb22567253bc3b8065b55
kuznetz <s.kuznetzov@mail.ru>
2017-03-12 12:15:36 +0300

    model collbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
 	validates :price,numericality: {greater_than:0,allow_nil: true}
 	validates :name, :description,presence: true
-  # attr_accessible :price,:name,:real,:weight,:description
+
+    after_initialize {}
+    after_save       {}
+    after_create     { category.inc(:item_count,1)}
+    after_update     {}
+    after_destroy    { category.inc(:items_count,-1)}
 end
