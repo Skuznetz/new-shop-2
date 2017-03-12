5b6912989b9f071b4ac4fbf835c3229e8ee959bf
kuznetz <s.kuznetzov@mail.ru>
2017-03-12 18:14:40 +0300

    method item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,14 @@ class ItemsController < ApplicationController
   	@items = Item.all 
   	render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
   end
+  def create
+  	@item = Item.create(item_params)
+  	
+  	render text: "#{@item.id}: #{@item.name}(#{!@item.new_record?})"
+  end
+  private
+
+  def item_params
+  	params.require(:item).permit(:name,:real,:weight,:description,:price)
+  end
 end
