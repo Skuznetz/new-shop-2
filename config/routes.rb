048e268974490ea26ba62a0925f64c0f7b4f4d72
kuznetz <s.kuznetzov@mail.ru>
2017-03-15 17:34:17 +0300

    router.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 	# get 'items/create'
 	# get 'items/index'
-	resources :items
+	resources :items do
+	  get :upvote,on: :member
+	end           
   
    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
