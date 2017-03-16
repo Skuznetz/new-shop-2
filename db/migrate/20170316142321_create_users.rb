40be3d3300a2eb5424e4ffe4feb6fd92a9b936c1
kuznetz <s.kuznetzov@mail.ru>
2017-03-16 19:05:02 +0300

    has_one ,belongs_to

--- db/migrate/20170316142321_create_users.rb
+++ db/migrate/20170316142321_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
-      t.timestamps
+       t.string :login
+      t.timestamps 
     end
   end
 end
