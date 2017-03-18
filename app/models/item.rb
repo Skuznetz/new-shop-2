5600d9059f834df744fa6b12c515de0f433fa9d9
kuznetz <s.kuznetzov@mail.ru>
2017-03-18 09:33:11 +0300

    has_and_belongs_to_many cart and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
 	validates :price,numericality: {greater_than:0,allow_nil: true}
 	validates :name, :description,presence: true
+
+	has_and_belongs_to_many :carts
    # attr_acessible :price,:name,:real,:weight,:description
     # after_initialize {}
     # after_save       {}
