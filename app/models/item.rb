5ada957c475cbe8f8ef19036f4cd6770d6bc9ca6
kuznetz <s.kuznetzov@mail.ru>
2017-03-18 19:19:04 +0300

    commentable

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,8 @@ class Item < ApplicationRecord
 	
 	has_many :positions
 	has_many :carts,through: :positions
+
+	has_many :comments,as: :commentable
    # attr_acessible :price,:name,:real,:weight,:description
     # after_initialize {}
     # after_save       {}
