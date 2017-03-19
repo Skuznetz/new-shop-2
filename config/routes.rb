0695cbf487d4202c77ec4c1c96a382db02143506
kuznetz <s.kuznetzov@mail.ru>
2017-03-19 14:43:42 +0300

    admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 	resources :items do
 	  get :upvote,on: :member
 	  get :expensive, on: :collection
-	end           
+	end    
+
+	get "admin/users_count" => "admin#users_count"      
   
    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
