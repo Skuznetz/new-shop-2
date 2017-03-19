eef16d96e32d0c5aa6e7b29b795425bf526c9659
kuznetz <s.kuznetzov@mail.ru>
2017-03-19 19:23:34 +0300

    votes_from

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,7 @@ class ItemsController < ApplicationController
   	@items = Item
   	@items = @items.where(" price >=?",params[:price_from]) if params[:price_from]
   	@items =@items.where("created_at >= ?",1.day.ago) if params[:today]
+  	@items = @items.where("votes_count >=?",params[:votes_from]) if params[:votes_from]
   	@items = @items.order("votes_count DESC","price")
   end
 
