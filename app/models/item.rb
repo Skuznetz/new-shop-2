ba3dd28283f7d446c1c3a8c3fd551a325b44ad88
kuznetz <s.kuznetzov@mail.ru>
2017-03-20 16:58:00 +0300

    model image

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
 	has_many :carts,through: :positions
 
 	has_many :comments,as: :commentable
+	
    # attr_acessible :price,:name,:real,:weight,:description
     # after_initialize {}
     # after_save       {}
