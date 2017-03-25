7d0e89ec93f1b82fe278428bb4b1c21c593e4b78
Skuznetz <s.kuznetzov@mail.ru>
2017-03-25 11:23:34 +0200

    add order_spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324154539) do
+ActiveRecord::Schema.define(version: 20170325082254) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170324154539) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "total"
   end
 
   create_table "positions", force: :cascade do |t|
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe Order do 
+  it "расчет полной стоимости заказа" do 
+  	item1 = Item.new(price: 10)
+    item2 = Item.new(price: 20)
+
+    order = Order.new
+    order.items << item1
+    order.items << item2
+
+    order.calculate_total
+    order.total.should = 30
+end
+end
