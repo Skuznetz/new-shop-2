72acc967cecb66e72fe460c8b76b69d52a25d961
kuznetz <s.kuznetzov@mail.ru>
2017-03-26 21:02:24 +0300

    describe "show action"

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -4,5 +4,10 @@ require 'rails_helper'
 describe ItemsController do 
 
   describe "show action"
+    it "renders show template if an items is found" do 
+      item = create(:item)
+      get :show,{ id: item.id }
+  end
+  
 
 end
